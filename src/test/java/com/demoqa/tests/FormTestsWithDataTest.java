0efc19d8d387dbdadfaa08ce271386ed59af5ab7
3007ver <veronika.shyshlo@gmail.com>
2023-05-23 18:49:31 +0300

    added Allure and remote tests

--- src/test/java/com/demoqa/tests/FormTestsWithDataTest.java
+++ src/test/java/com/demoqa/tests/FormTestsWithDataTest.java
@@ -1,41 +1,49 @@
 package com.demoqa.tests;
 
 import com.demoqa.pages.RegistrationPage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.demoqa.tests.TestData.*;
+import static io.qameta.allure.Allure.step;
 
 public class FormTestsWithDataTest extends TestBase {
     RegistrationPage registrationPage = new RegistrationPage();
     @Test
+    @Tag("remote")
+    @DisplayName("Successful registration")
     void fillFormTests () {
-        registrationPage.openPage()
-                .removeHeaderAndFooter()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setUserEmail(userEmail)
-                .setGender(gender)
-                .setUserNumber(userNumber)
-                .setBirthDate(dayOfBirth, monthOfBirth, yearOfBirth)
-                .setSubjects(subjects)
-                .setHobbies(hobby)
-                .uploadPicture(picture)
-                .setAdress(currentAddress)
-                .setState(state)
-                .setCity(city)
-                .clickSubmitButton();
+        step("Open and fill Registration Form", () -> {
+                    registrationPage.openPage()
+                            .removeHeaderAndFooter()
+                            .setFirstName(firstName)
+                            .setLastName(lastName)
+                            .setUserEmail(userEmail)
+                            .setGender(gender)
+                            .setUserNumber(userNumber)
+                            .setBirthDate(dayOfBirth, monthOfBirth, yearOfBirth)
+                            .setSubjects(subjects)
+                            .setHobbies(hobby)
+                            .uploadPicture(picture)
+                            .setAdress(currentAddress)
+                            .setState(state)
+                            .setCity(city)
+                            .clickSubmitButton();
+                });
 
-        registrationPage.verifyModalsAppear()
-                .verifyResults("Student Name", firstName + " " + lastName)
-                .verifyResults("Student Email", userEmail)
-                .verifyResults("Gender", gender)
-                .verifyResults("Mobile", userNumber)
-                .verifyResults("Date of Birth", String.format("%s %s,%s", dayOfBirth, monthOfBirth, yearOfBirth))
-                .verifyResults("Subjects", subjects)
-                .verifyResults("Hobbies", hobby)
-                .verifyResults("Picture", picture)
-                .verifyResults("Address", currentAddress)
-                .verifyResults("State and City", state + " " + city);
-
+        step("Verify results", () -> {
+            registrationPage.verifyModalsAppear()
+                    .verifyResults("Student Name", firstName + " " + lastName)
+                    .verifyResults("Student Email", userEmail)
+                    .verifyResults("Gender", gender)
+                    .verifyResults("Mobile", userNumber)
+                    .verifyResults("Date of Birth", String.format("%s %s,%s", dayOfBirth, monthOfBirth, yearOfBirth))
+                    .verifyResults("Subjects", subjects)
+                    .verifyResults("Hobbies", hobby)
+                    .verifyResults("Picture", picture)
+                    .verifyResults("Address", currentAddress)
+                    .verifyResults("State and City", state + " " + city);
+        });
     }
 }
--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -1,17 +1,43 @@
 package com.demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.demoqa.helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 public class TestBase {
 
    @BeforeAll
-    static void BeforeAll () {
-       Configuration.baseUrl = "https://demoqa.com";
-       Configuration.browserSize = "1920x1080";
-       Configuration.browser = "chrome";
-       Configuration. pageLoadStrategy="eager";
-       Configuration.holdBrowserOpen = false;
+    static void beforeAll () {
+      Configuration.baseUrl = "https://demoqa.com";
+      Configuration.browserSize = "1920x1080";
+      //  Configuration.browser = "chrome";
+      Configuration.pageLoadStrategy = "eager";
 
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+              "enableVNC", true,
+              "enableVideo", true
+      ));
+
+      Configuration.browserCapabilities = capabilities;
+   }
+   @BeforeEach
+   void addListener() {
+      SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+   }
+
+   @AfterEach
+   void addAttachments () {
+      Attach.screenshotAs("Last screenshot");
+      Attach.pageSource();
+      Attach.browserConsoleLogs();
+      Attach.addVideo();
    }
 }
